251676658c9d3db22616fe0593dfa8a45cf39e03
Aaron Iles <aaron.iles@gmail.com>
2013-05-28 21:46:20 +1000

    Update cffi dependency to 0.6
    Update depedency requirement to latest cffi release.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Networking'
     ],
-    install_requires=['cffi==0.5'],
+    install_requires=['cffi==0.6'],
     tests_require=['mock'] + [] if PYTHON3K else ['unittest2'],
     test_suite="tests" if PYTHON3K else "unittest2.collector"
 )
